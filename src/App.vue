0e95d5e92bddf45b9f05dcdbf4b119dd9fb0e6ce
Danny Roberts <danzibob@gmail.com>
2022-05-15 19:04:13 +0100

    Add p5 to the project, using instanced mode

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CanvasComponent msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CanvasComponent from './components/CanvasComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CanvasComponent
   }
 }
 </script>
---
+++ src/components/CanvasComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CanvasComponent',
+  props: {
+    msg: String
+  },
+  mounted() {
+    const script = (p5) => {
+      p5.setup = () => {
+        // Clean up old canvas (only needed in dev)
+        let existingCanvas = document.getElementsByTagName("canvas")
+        existingCanvas.remove()
+        
+        // Create new canvas
+        p5.createCanvas(400,400)
+      }
+
+      p5.draw = () => {
+        p5.background(51);
+        p5.fill(0)
+        p5.circle(p5.mouseX, p5.mouseY, 50)
+      }
+    }
+
+    const P5 = require("p5")
+    new P5(script)
+  }
+}
+</script>
+
+<style>
+
+</style>
